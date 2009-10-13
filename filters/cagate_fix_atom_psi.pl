e42cbbcfa81fb5c1aabd9ec72e24f7cc19b88f7a
Michael Goerz <goerz@physik.fu-berlin.de>
2009-10-14 01:00:44 +0200

    Added filter for cagate atomic prop. wavefunctions
    The propagated wave functions from the CaGate program are not easy to
    plot. This filter rewrites the data file into a better format

---
+++ filters/cagate_fix_atom_psi.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $usage="fix_atomic_prop_psi.pl DATFILE\n";
+
+if (@ARGV >= 1){
+    my $datfile = $ARGV[0];
+    open(STDIN, $datfile) or die("Couldn't open $datfile\n");
+}
+
+print "#       time[au]            Re[Psi]             Im[Psi]            |Psi|^2              phase\n";
+my $time = 0.0;
+foreach my $line (<STDIN>){
+    if ($line =~ m'^\s*#\s*time\s*=\s+([+\-\d.EDed]+)\s*$'){
+        $time = $1;
+        $time = sprintf("%-20s", $time);
+    } elsif ($line =~ m'^\s*([+\-\d.EDed]+)\s+([+\-\d.EDed]+)\s+([+\-\d.EDed]+)\s+([+\-\d.EDed]+)\s+([+\-\d.EDed]+)') {
+        my $r = $1;
+        my $re_psi = $2;
+        $re_psi = sprintf("%-20s", $re_psi);
+        my $im_psi = $3;
+        $im_psi = sprintf("%-20s", $im_psi);
+        my $sq_psi = $4;
+        $sq_psi = sprintf("%-20s", $sq_psi);
+        my $phase  = $5;
+        $phase = sprintf("%-20s", $phase);
+        print "$time $re_psi $im_psi $sq_psi $phase\n";
+    }
+}
+
+close STDIN;
