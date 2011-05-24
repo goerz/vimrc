bfaeacf0fe28e700563b80ae86f59bce41fc9566
Michael Goerz <goerz@physik.uni-kassel.de>
2011-05-24 21:29:04 +0200

    Improved Fortran routine snippet

--- snippets/fortran/routine.f90
+++ snippets/fortran/routine.f90
@@ -2,14 +2,8 @@
   !! @param: 
   subroutine <+routinename+>
 
-    type(pulse_t),     intent(inout) :: pulse
-
-    integer :: i
-
     character (len=error_l) :: routine_name = '<+routinename+>'
     call add_backtrace(module_name, routine_name)
-    
-    <++>
 
     call del_backtrace()
 
