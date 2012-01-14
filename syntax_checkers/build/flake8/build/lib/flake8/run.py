9cb0fa59dcb45263be1035ddf7d100cc14183a89
Michael Goerz <goerz@physik.uni-kassel.de>
2012-01-14 17:43:52 +0100

    Added syntastic plugin

---
+++ syntax_checkers/build/flake8/build/lib/flake8/run.py
@@ -0,0 +1,104 @@
+
+"""
+Implementation of the command-line I{flake8} tool.
+"""
+import sys
+import os
+import os.path
+
+from flake8.util import skip_file
+from flake8 import pep8
+from flake8 import pyflakes
+from flake8 import mccabe
+
+
+def check_file(path, complexity=-1):
+    warnings = pyflakes.checkPath(path)
+    warnings += pep8.input_file(path)
+    if complexity > -1:
+        warnings += mccabe.get_module_complexity(path, complexity)
+    return warnings
+
+
+def check_code(code, complexity=-1):
+    warnings = pyflakes.check(code, '<stdin>')
+    if complexity > -1:
+        warnings += mccabe.get_code_complexity(code, complexity)
+    return warnings
+
+
+def _get_python_files(paths):
+    for path in paths:
+        if os.path.isdir(path):
+            for dirpath, dirnames, filenames in os.walk(path):
+                for filename in filenames:
+                    if not filename.endswith('.py'):
+                        continue
+                    fullpath = os.path.join(dirpath, filename)
+                    if not skip_file(fullpath):
+                        yield fullpath
+
+        else:
+            if not skip_file(path):
+                yield path
+
+
+def main():
+    options, args = pep8.process_options()
+    complexity = options.max_complexity
+    warnings = 0
+    if args:
+        for path in _get_python_files(args):
+            warnings += check_file(path, complexity)
+    else:
+        stdin = sys.stdin.read()
+        warnings += check_code(stdin, complexity)
+
+    raise SystemExit(warnings > 0)
+
+
+def _get_files(repo, **kwargs):
+    seen = set()
+    for rev in xrange(repo[kwargs['node']], len(repo)):
+        for file_ in repo[rev].files():
+            file_ = os.path.join(repo.root, file_)
+            if file_ in seen or not os.path.exists(file_):
+                continue
+            seen.add(file_)
+            if not file_.endswith('.py'):
+                continue
+            if skip_file(file_):
+                continue
+            yield file_
+
+
+class _PEP8Options(object):
+    exclude = select = []
+    show_pep8 = show_source = quiet = verbose = testsuite = False
+    no_repeat = False
+    counters = {}
+    messages = {}
+    ignore = pep8.DEFAULT_IGNORE
+
+
+def hg_hook(ui, repo, **kwargs):
+    # default pep8 setup
+    pep8.options = _PEP8Options()
+    pep8.options.physical_checks = pep8.find_checks('physical_line')
+    pep8.options.logical_checks = pep8.find_checks('logical_line')
+    pep8.args = []
+    complexity = ui.configint('flake8', 'complexity', default=-1)
+    warnings = 0
+
+    for file_ in _get_files(repo, **kwargs):
+        warnings += check_file(file_, complexity)
+
+    strict = ui.configbool('flake8', 'strict', default=True)
+
+    if strict:
+        return warnings
+
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ syntax_checkers/build/flake8/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+README = open('README').read()
+
+setup(
+    name="flake8",
+    license="MIT",
+    version="1.0",
+    description="code checking using pep8 and pyflakes",
+    author="Tarek Ziade",
+    author_email="tarek@ziade.org",
+    url="http://bitbucket.org/tarek/flake8",
+    packages=["flake8", "flake8.tests"],
+    scripts=["bin/flake8"],
+    long_description=README,
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        ])
